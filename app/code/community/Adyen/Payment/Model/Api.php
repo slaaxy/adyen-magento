a193124d6241c9839143d64a2e3d25a13352ec15
alessio <alessio.zampatti@adyen.com>
2019-05-30 18:22:11 +0800

    Add ApplicationInfo to /payments

--- app/code/community/Adyen/Payment/Model/Api.php
+++ app/code/community/Adyen/Payment/Model/Api.php
@@ -122,7 +122,7 @@ class Adyen_Payment_Model_Api extends Mage_Core_Model_Abstract
         $request['shopperEmail'] = $customerEmail;
         $request['shopperIP'] = $order->getRemoteIp();
         $request['shopperReference'] = !empty($customerId) ? $customerId : self::GUEST_ID . $realOrderId;
-
+        $request['applicationInfo'] = array(new Adyen_Payment_Model_Adyen_Data_ApplicationInfo());
 
         $request = $this->buildAddressData($request, $billingAddress, $deliveryAddress);
         $request = $this->setRecurringMode($request, $paymentMethod, $payment, $storeId);
